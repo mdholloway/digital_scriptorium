53f2c1e1071b2f59db5e79519d98d54540a17bb9
Michael Holloway <michael@mdholloway.org>
2025-01-11 15:41:19 -0500

    More facet fixes

--- spec/digital_scriptorium/unqualified_claim_transformer_spec.rb
+++ spec/digital_scriptorium/unqualified_claim_transformer_spec.rb
@@ -14,7 +14,8 @@ module DigitalScriptorium
       claim = StatementRepresenter.new(Statement.new).from_json(json)
       expected = {
         'holding_status_display' => ['{"recorded_value":"Current"}'],
-        'holding_status_search' => ['Current']
+        'holding_status_search' => ['Current'],
+        'holding_status_facet' => ['Current']
       }
 
       it 'provides the label for the canonical holding status item in the display property' do
@@ -28,7 +29,8 @@ module DigitalScriptorium
       claim = StatementRepresenter.new(Statement.new).from_json(json)
       expected = {
         'shelfmark_display' => ['{"recorded_value":"Oversize LJS 110"}'],
-        'shelfmark_search' => ['Oversize LJS 110']
+        'shelfmark_search' => ['Oversize LJS 110'],
+        'shelfmark_facet' => ['Oversize LJS 110']
       }
 
       it 'provides the recorded shelfmark in the display and search properties' do
@@ -42,7 +44,8 @@ module DigitalScriptorium
       claim = StatementRepresenter.new(Statement.new).from_json(json)
       expected = {
         'physical_description_display' => ['{"recorded_value":"Extent: 1 parchment ; 170 x 245 mm."}'],
-        'physical_description_search' => ['Extent: 1 parchment ; 170 x 245 mm.']
+        'physical_description_search' => ['Extent: 1 parchment ; 170 x 245 mm.'],
+        'physical_description_facet' => ['Extent: 1 parchment ; 170 x 245 mm.']
       }
 
       it 'provides the recorded physical description in the display and search fields' do
