c32abe118cc2b2ff3bf9e27300551771346d2fad
Michael Holloway <michael@mdholloway.org>
2025-02-02 15:56:54 -0500

    Add constants for Item IDs through Q17

--- lib/digital_scriptorium/item_id.rb
+++ lib/digital_scriptorium/item_id.rb
@@ -5,10 +5,23 @@ require 'set'
 module DigitalScriptorium
   # Constants for core model item IDs.
   module ItemId
-    MANUSCRIPT             = 'Q1'
-    HOLDING                = 'Q2'
-    RECORD                 = 'Q3'
-    HOLDING_STATUS_CURRENT = 'Q4'
+    MANUSCRIPT                 = 'Q1'
+    HOLDING                    = 'Q2'
+    RECORD                     = 'Q3'
+    HOLDING_STATUS_CURRENT     = 'Q4'
+    HOLDING_STATUS_NON_CURRENT = 'Q5'
+    STANDARD_TITLE             = 'Q6'
+    ACTOR                      = 'Q7'
+    PERSONAL_NAME              = 'Q8'
+    CORPORATE_NAME             = 'Q9'
+    ROLE                       = 'Q10'
+    TERM                       = 'Q11'
+    LANGUAGE                   = 'Q12'
+    CENTURY                    = 'Q13'
+    DATED                      = 'Q14'
+    UNDATED                    = 'Q15'
+    PLACE                      = 'Q16'
+    MATERIAL                   = 'Q17'
 
     CORE_MODEL_ITEMS = Set[MANUSCRIPT, HOLDING, RECORD]
   end
